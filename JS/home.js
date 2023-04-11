1dc8bd18a8155f44550fbea972aba3430abef61c
Kareem sakkary <kareemsakkary101@gmail.com>
2023-04-11 18:11:12 +0200

    ..

--- JS/home.js
+++ JS/home.js
@@ -2,4 +2,6 @@ menu = document.querySelector(".menu");
 menu.onclick = function () {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
+    header = document.querySelector("header");
+    header.classList.toggle("active");
 }
